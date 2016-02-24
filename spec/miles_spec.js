cc5d8480c90259fb9f87763ecc89a6eb44922070
Nathan Reginato <nathan.reginato@gmail.com>
2016-02-24 11:27:16 -0700

    Added inital test

---
+++ spec/miles_spec.js
@@ -0,0 +1,7 @@
+var miles = require('../lib/miles')
+
+describe('miles', function() {
+  it('is our master plan', function() {
+    expect(someFunction()).toEqual("running stuff foreal")
+  });
+});
