6b49c68752402755bad9bea76f50056187304764
Nathan Reginato <nathan.reginato@gmail.com>
2016-02-24 11:33:53 -0700

    Added first test. Fails as expected

--- spec/miles_spec.js
+++ spec/miles_spec.js
@@ -2,6 +2,6 @@ var miles = require('../lib/miles')
 
 describe('miles', function() {
   it('is our master plan', function() {
-    expect(someFunction()).toEqual("running stuff foreal")
+    expect(miles.someFunction()).toEqual("running stuff foreal")
   });
 });
